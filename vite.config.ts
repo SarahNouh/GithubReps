4858c32cb294aa2f004210515542ff026b3e922c
Sarah Nouh <sarah.nouh@robustastudio.com>
2024-06-12 04:17:35 +0300

    chore: update postcss setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}), // add options if needed
+        postcssNested(),
+      ],
+    },
+  },
+});
